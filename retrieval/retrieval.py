61f13a28445c765718dd52f2bbb2acb581efaf7c
k391160334 <maseqqqq@gmail.com>
2022-05-15 13:24:20 +0900

    retrieval top5 similar article url

---
+++ retrieval/retrieval.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def get_tf_idf_query_similarity(vectorizer,docs_tfidf,query):
+	query_tfidf = vectorizer.transform([query])
+	cosineSimilarities = cosine_similarity(query_tfidf, docs_tfidf).flatten()
+	return cosineSimilarities
+
+
+input_file="다음뉴스_20220501_20220509_크롤링.csv"
+df = pd.read_csv(input_file, header = 0)
+documents=df['본문'].values.astype('U')
+
+#vectorizer = TfidfVectorizer(preprocessor=nlp.clean_tf_idf_text)
+vectorizer = TfidfVectorizer()
+docs_tfidf = vectorizer.fit_transform(documents)
+
+#input query by console input, input 끝	to terminate
+import sys
+read=sys.stdin.readline
+print('ready...')
+while True:
+	query=read().rstrip()
+	if query=='끝':
+		break
+	cos_sim=get_tf_idf_query_similarity(vectorizer,docs_tfidf,query)
+	cos_sim_item=sorted([(sim,i) for i,sim in enumerate(cos_sim)],reverse=True)
+	
+	for sim,i in cos_sim_item[:5]:
+		print(df.iloc[i,1], sim)
+
+
+
+#eval:mAP
